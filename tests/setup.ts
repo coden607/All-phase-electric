b0e7b35185db41da430536b4f174a9e0fae50bd3
coden607 <coden607@gmail.com>
2026-09-02 11:51:10 -0400

    test: provide ResizeObserver in jsdom

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom/vitest';
+
+class ResizeObserverStub {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+if (!globalThis.ResizeObserver) {
+  Object.defineProperty(globalThis, 'ResizeObserver', {
+    configurable: true,
+    writable: true,
+    value: ResizeObserverStub,
+  });
+}
